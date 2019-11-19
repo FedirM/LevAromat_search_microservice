a7cf889910b70a37fcf4e7c289b9b961e0a77cc7
FedirM <tedmoster@gmail.com>
2019-11-19 19:38:43 +0200

    CORS

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ function reflect( promise ) {
 
 
 app.post('/search', function (req, res) {
+    res.header('Access-Control-Allow-Credentials', true);
     if( !req.query.q || !req.query.priceType ) return res.sendStatus(400);
     reflect( new Promise((resolve, reject) => {
         mysqlConnection.query(`SELECT product.product_id, product.name FROM product, prices
